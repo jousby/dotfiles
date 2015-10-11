de1aa00f801057a2741b3a5bbc31de81bd4c6353
James Ousby <jousby@gmail.com>
2015-10-11 17:50:00 +1100

    adding functions file

---
+++ zsh-custom/my-functions.zsh
@@ -0,0 +1,22 @@
+function new-alias () {
+  if [ -z "$1" ]; then
+    echo "alias name:"
+    read NAME
+  else
+    NAME=$1
+  fi
+  
+  if [ -z "$2" ]; then
+    echo "alias definition:"
+    read DEFINTION
+  else
+    if [ "$2" = "-cd" ]; then
+      DEFINTION='cd '
+    else
+      DEFINTION=$2
+    fi
+  fi
+
+  echo "alias $NAME='$DEFINTION'" >> ~/.bash_alias
+														      . ~/.bash_alias
+}
